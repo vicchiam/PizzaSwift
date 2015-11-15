8327dc383c7234f92b564b468a18a2b846b1acfc
vicchiam <vicchiam81@gmail.com>
2015-11-15 10:36:06 +0100

    Project created

---
+++ PizzaSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PizzaSwift
+//
+//  Created by Victor Chisvert Amat on 15/11/15.
+//  Copyright © 2015 Vicchiam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
